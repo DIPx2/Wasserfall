1cc06c59f61c1a93e6060da42d5ee169076198ef
Rygor-XVII <robot.7b2@gmail.com>
2025-08-28 16:06:53 +0300

    Добавлена папка Feuerlilie и удалены устаревшие файлы

--- SpareParts/brdk/scratch_8.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM "groups"  ORDER BY 1 DESC ;
-
-EXPLAIN SELECT ;
-
-EXPLAIN update "messages" set "remove_status" = 't' where ("profile_id" = '1193793') and ("template_id" = '1318') and remove_status is not true;
--- SpareParts/events.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO events.event_log(id, event_date, user_id, description)
-	VALUES (DEFAULT, DEFAULT, DEFAULT, 'Подключение prd-bi-04.maxbit.private к реиндексации');
